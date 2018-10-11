28e6c7ed8f6a2ec5d63abde5fb0edc55e7b2ad4b
Jesper Jensen sverd <jesperjensensverd@Jespers-MacBook-Pro.local>
2018-10-11 10:41:15 +0200

    update whole file

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+// CANVAS SIZE & CALLING ID="CANVAS"
 var canvas = document.getElementById('canvas'),
 	c = canvas.getContext('2d');
 var innerWidth = 360,
 	innerHeight = 620;
 	canvas.width = innerWidth,
 	canvas.height = innerHeight;
+
+
+// KEY MAPPING 
