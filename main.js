eb5cf0652ffca849fd0075a3642f8085759180c9
Jesper Jensen sverd <jesperjensensverd@Jespers-MacBook-Pro.local>
2018-10-11 10:32:08 +0200

    js update 1

---
+++ main.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('canvas'),
+	c = canvas.getContext('2d');
+var innerWidth = 360,
+	innerHeight = 620;
+	canvas.width = innerWidth,
+	canvas.height = innerHeight;
